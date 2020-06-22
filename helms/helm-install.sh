02a238ad42b750f546da3972b9e7e38cb2e521b3
puhaiyang <761396462@qq.com>
2020-06-23 00:04:15 +0800

    1.暂存

--- helms/helm-install.sh
+++ helms/helm-install.sh
@@ -5,8 +5,6 @@ project_git_url=https://github.com/puhaiyang/spring-boot-cloud.git
 image_domain=ccr.ccs.tencentyun.com/spring-boot-cloud
 #项目的根目录
 git_root_path=~/git
-#项目目录
-git=$git_root_path/$pname
 #通用chart目录
 chart_root_path=~/git/helms
 #版本号,docker的tag
@@ -22,6 +20,7 @@ if [ ! -d $git_root_path ];then
 fi
 ########################初始化 end###########################
 source /etc/profile
+#自定义分支名
 if [ -z "$1" ]; then
     #默认分支名称
     branch=master
@@ -33,46 +32,54 @@ echo '----------------------------------------------'
 echo ':-D hi,boy!welcome to automatic depoly sh! :-D'
 echo '----------------------------------------------'
 
+
 echo -e "branch:$branch, 系统服务列表:"
+################################列目录 begin############################
+pom_files=$(dirname $(find git/spring-boot-cloud/ -name pom.xml))
+echo $pom_files
+igrone_pro="spring-boot-cloud"
+index=0
+
+for i in ${pom_files[@]};do
+  pro_name=$(basename $i)
+  if [ "$pro_name" = "$igrone_pro" ]; then
+     continue
+  fi
+  ((index++))
+  echo $index')'$pro_name
+done
+################################列目录 end############################
+
+
+
 ls $git_root_path/  |grep "spring-boot-cloud"
 echo -e "请输入模块名称(请输入spring-boot-cloud-xxx后的xxx即可):"
 read pname
 pname="spring-boot-cloud-$pname"
 echo "更新的模块：$pname"
-
+#项目目录
+git=$git_root_path/$pname
 #切换到项目的目录
 cd $git
+#获取到新的代码
 git fetch --all
 git reset --hard origin/$branch
-
+#获取到最后一次提交的内容
 git_last_commit=$(git show -s --format=%s)
+#获取到最后一次提交者
 git_last_commit_author=$(git show -s --format=%an)
-
-echo '统一处理日志文件(logback-spring.xml)配置...'
-sed "s/module/$pname/g" ~/config/logback_template.xml > $git/src/main/resources/logback-spring.xml
-echo '统一处理启动(bootstrap.properties)配置...'
-sed "s/module/$pname/g" ~/config/spring-boot-cloud-template.properties > $git/src/main/resources/bootstrap.properties
-#项目是maven聚合项目，需要在git的根目录进打包,-pl --projects <arg> 构建制定的模块，模块间用逗号分隔
+#如果项目是maven聚合项目，需要在git的根目录进打包,-pl --projects <arg> 构建制定的模块，模块间用逗号分隔
 #-am --also-make 同时构建所列模块的依赖模块；
 cd $git_root_path
-
 echo '下面开始打包(package)...'
 mvn clean  package -Dmaven.test.skip=true -pl $pname -am
 #mvn clean  package -Dmaven.test.skip=true
 echo '好,包打好了！接下来准备打镜像！(docker image package)...'
 cd $git
-if [ "$pname" == "xxxxxxx" ]; then
-	sed "s/module/$pname/g" ~/config/Dockerfile-xxxxxxxxx-with-skywalking-template  > target/Dockerfile
-	echo "$pname will update"
-else
-	sed "s/module/$pname/g" ~/config/Dockerfile-with-skywalking-template  > target/Dockerfile
-	echo "$pname 统一处理dockerFile完毕(docker file copy)"
-fi
-
-echo '现在拷贝时区文件'
-cp ~/config/TimeZone target/
-image=$image_domain/$pname:$version
+#docker镜像名
 docker_image=$image_domain/$pname
+#docker镜像名
+image=$docker_image:$version
 echo '现在开始打镜像...'
 cd target
 docker build -f  Dockerfile -t $image .
@@ -83,6 +90,7 @@ echo '好！现在准备发布更新了(pre deploy...)'
 echo '现在准备chart文件,通过通用chart文件来生成'
 path_common_chart=$chart_root_path/common-modle-springbootcloud
 echo $path_common_chart
+#在项目的target目录下创建chart目录
 mkdir chart
 echo '复制到target下的chart目录下'
 cp -r $path_common_chart chart
