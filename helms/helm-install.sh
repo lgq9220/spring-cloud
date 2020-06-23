2afd3cd0e0404fe0de1d0b2d9e2b4693fc4dd1f4
puhaiyang <puhaiyang@healthych.com>
2020-06-23 14:29:46 +0800

    1.修改helm文件

--- helms/helm-install.sh
+++ helms/helm-install.sh
@@ -3,10 +3,14 @@
 project_git_url=https://github.com/puhaiyang/spring-boot-cloud.git
 #docker镜像仓库地址
 image_domain=ccr.ccs.tencentyun.com/spring-boot-cloud
-#项目的根目录
+#项目名称
+project_name=spring-boot-cloud
+#所有项目的git根目录
 git_root_path=~/git
+#项目的根目录
+project_root_path=~/git/$project_name
 #通用chart目录
-chart_root_path=~/git/helms
+chart_root_path=$project_root_path/helms
 #版本号,docker的tag
 version=`date +%Y%m%d%H%M`
 ################### 配置信息end ###################
@@ -32,14 +36,12 @@ echo '----------------------------------------------'
 echo ':-D hi,boy!welcome to automatic depoly sh! :-D'
 echo '----------------------------------------------'
 
-
 echo -e "branch:$branch, 系统服务列表:"
 ################################列目录 begin############################
 pom_files=$(dirname $(find git/spring-boot-cloud/ -name pom.xml))
-echo $pom_files
 igrone_pro="spring-boot-cloud"
 index=0
-
+projects[0]="unknown"
 for i in ${pom_files[@]};do
   pro_name=$(basename $i)
   if [ "$pro_name" = "$igrone_pro" ]; then
@@ -47,18 +49,17 @@ for i in ${pom_files[@]};do
   fi
   ((index++))
   echo $index')'$pro_name
+  projects[${#projects[*]}]="$pro_name"
 done
+echo -e "请输入模块对应的序号:"
+read project_index
+echo ${projects[$project_index]}
+#要发布的项目名
+pname=${projects[$project_index]}
 ################################列目录 end############################
-
-
-
-ls $git_root_path/  |grep "spring-boot-cloud"
-echo -e "请输入模块名称(请输入spring-boot-cloud-xxx后的xxx即可):"
-read pname
-pname="spring-boot-cloud-$pname"
 echo "更新的模块：$pname"
-#项目目录
-git=$git_root_path/$pname
+#模块的目录
+git=$project_root_path/$pname
 #切换到项目的目录
 cd $git
 #获取到新的代码
@@ -70,7 +71,7 @@ git_last_commit=$(git show -s --format=%s)
 git_last_commit_author=$(git show -s --format=%an)
 #如果项目是maven聚合项目，需要在git的根目录进打包,-pl --projects <arg> 构建制定的模块，模块间用逗号分隔
 #-am --also-make 同时构建所列模块的依赖模块；
-cd $git_root_path
+cd $project_root_path
 echo '下面开始打包(package)...'
 mvn clean  package -Dmaven.test.skip=true -pl $pname -am
 #mvn clean  package -Dmaven.test.skip=true
@@ -81,7 +82,6 @@ docker_image=$image_domain/$pname
 #docker镜像名
 image=$docker_image:$version
 echo '现在开始打镜像...'
-cd target
 docker build -f  Dockerfile -t $image .
 echo '打好了，把镜像推到docker镜像仓库里去(push...)'
 docker push $image
@@ -101,14 +101,26 @@ sed -i "s/common-modle-springbootcloud/$pname/g" chart/$pname/Chart.yaml
 echo '替换values'
 sed -i "s/common-modle-springbootcloud/$pname/g" chart/$pname/values.yaml
 
-#install upgrade
+#install or upgrade
+###############判断是否已发布对应的项目 begin#####################
+helm_ops='install'
+echo helm list -A |grep $pname
+if [ $? -ne 0 ];then
+  echo "不存在"$pname"模块，将先进行install"
+  helm_ops="install"
+else
+  echo "已存在"$pname"模块，将先进行upgrade"
+  helm_ops="upgrade"
+fi
+###############判断是否已发布对应的项目 end#####################
+
 echo '进行发布(deploy...)'
 if [ -f "$chart_root_path/values/$pname/values.yaml" ];then
     echo '已经有配置文件了，采用配置文件进行发布'
-	helm install $pname chart/$pname -n nnnnnnnnn -f $chart_root_path/values/$pname/values.yaml --set image.tag=$version --set image.repository=$docker_image
+	helm $helm_ops $pname chart/$pname -n nnnnnnnnn -f $chart_root_path/values/$pname/values.yaml --set image.tag=$version --set image.repository=$docker_image
 else
     echo '直接进行发布'
-    helm install $pname chart/$pname -n nnnnnnnnn --set image.tag=$version --set image.repository=$docker_image
+    helm $helm_ops $pname chart/$pname -n nnnnnnnnn --set image.tag=$version --set image.repository=$docker_image
 fi
 
 Message="发布通知: \n模块:${pname} \ngit提交者:$git_last_commit_author \ngit提交内容:$git_last_commit \n------have a nice day------"
